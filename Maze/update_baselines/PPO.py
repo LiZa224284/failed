09a581ec3cba1fbfd29cea14ad1d2a16c07e4c17
EndlessCG <2656434088@qq.com>
2024-12-26 03:37:57 -0600

    update

--- Maze/update_baselines/PPO.py
+++ Maze/update_baselines/PPO.py
@@ -26,7 +26,7 @@ class Critic(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(state_dim, hidden_dim),
             nn.Linear(hidden_dim, hidden_dim),
-            nn.Linear(hidden_dim, action_dim),
+            nn.Linear(hidden_dim, 1),
         )
 
     def forward(self, state):
@@ -60,16 +60,21 @@ class ReplayBuffer():
         self.next_state = np.zeros((max_size, state_dim))
         self.reward = np.zeros((max_size, 1))
         self.done_bool = np.zeros((max_size, 1))
+        self.value = np.zeros((max_size, 1))
+        self.log_prob = np.zeros((max_size, 1))
 
         self.device = device
 
 
-    def add(self, state, action, next_state, reward, done_bool):
-        self.state[self.ptr] = state
-        self.action[self.ptr] = action
-        self.next_state[self.ptr] = next_state
-        self.reward[self.ptr] = reward
-        self.done_bool[self.ptr] = done_bool
+    def add(self, state, action, next_state, reward, done_bool, value, log_prob):
+        idx = self.ptr % self.max_size
+        self.state[idx] = state
+        self.action[idx] = action
+        self.next_state[idx] = next_state
+        self.reward[idx] = reward
+        self.done_bool[idx] = done_bool
+        self.value[idx] = value
+        self.log_prob[idx] = log_prob
 
         self.ptr = (self.ptr + 1) % self.max_size
         self.size = min(self.size + 1, self.max_size)
@@ -77,25 +82,23 @@ class ReplayBuffer():
 
     def sample(self, batch_size):
         # ind = np.random.randint(0, self.ptr, size=batch_size)
-        ind = np.random.randint(0, self.size, size=batch_size)
+        # ind = np.random.randint(0, self.size, size=batch_size)
         # effective_batch_size = min(batch_size, self.size)
         # ind = np.random.choice(self.size, size=effective_batch_size, replace=False)
+        size = min(self.ptr, self.max_size)
 
-        sampled_data =  (
-        torch.FloatTensor(self.state[ind]).to(self.device),
-        torch.FloatTensor(self.action[ind]).to(self.device),
-        torch.FloatTensor(self.reward[ind]).to(self.device),
-        torch.FloatTensor(self.next_state[ind]).to(self.device),
-        torch.FloatTensor(self.done_bool[ind]).to(self.device),
+        return (
+            torch.FloatTensor(self.state[:size]).to(self.device),
+            torch.FloatTensor(self.action[:size]).to(self.device),
+            torch.FloatTensor(self.reward[:size]).to(self.device),
+            torch.FloatTensor(self.next_state[:size]).to(self.device),
+            torch.FloatTensor(self.done_bool[:size]).to(self.device),
+            torch.FloatTensor(self.value[:size]).to(self.device),
+            torch.FloatTensor(self.log_prob[:size]).to(self.device),
         )
-    
-        # self.ptr = 0
-
-        return sampled_data
     
     def clear(self):
         self.ptr = 0
-        self.size = 0
 
 # 定义 TD3 算法
 class PPO:
@@ -121,12 +124,8 @@ class PPO:
 
         # Actor and Critic networks
         self.actor = Actor(state_dim, action_dim, max_action, hidden_dim=256).to(self.device)
-        # self.actor_target = Actor(state_dim, action_dim, max_action).to(self.device)
-        # self.actor_target = copy.deepcopy(self.actor)
-        # self.actor_target.load_state_dict(self.actor.state_dict())
-
         self.critic = Critic(state_dim, action_dim).to(self.device)
-        # self.critic_target = copy.deepcopy(self.critic)
+
 
         # Optimizers
         self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=actor_lr)
@@ -153,6 +152,15 @@ class PPO:
         state = torch.FloatTensor(state).to(self.device).unsqueeze(0)
         action = self.actor(state).cpu().data.numpy().flatten()
         return action
+    
+    def compute_gae(self, rewards, values, dones, next_values):
+        deltas = rewards + self.gamma * next_values * (1 - dones) - values
+        advantages = torch.zeros_like(deltas).to(self.device)
+        advantage = 0.0
+        for t in reversed(range(len(deltas))):
+            advantage = deltas[t] + self.gamma * self.lmbda * (1 - dones[t]) * advantage
+            advantages[t] = advantage
+        return advantages
 
     def train(self):
         self.total_it += 1
@@ -161,26 +169,31 @@ class PPO:
         self.critic_scheduler.step()
 
         # Sample batch from replay buffer
-        states, actions, rewards, next_states, done_bool = self.replay_buffer.sample(self.batch_size)
+        states, actions, rewards, next_states, done_bool, value, old_log_probs = self.replay_buffer.sample(self.batch_size)
+        # rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
+        next_values = torch.cat((value[1:], torch.zeros(1, 1).to(self.device)))
+        # next_values = self.critic(next_states)
+        advantages = self.compute_gae(rewards, value, done_bool, next_values)
+        td_target = rewards + self.gamma * next_values * (1 - done_bool)
 
-        td_target = rewards + self.gamma * self.critic(next_states) * (1 - done_bool)
-        td_delta = td_target - self.critic(states)
+        # td_target = rewards + self.gamma * self.critic(next_states) * (1 - done_bool)
+        # td_delta = td_target - self.critic(states)
 
-        advantages = []
-        advantage = 0.0
-        td_delta = td_delta.cpu().detach().numpy()  # Convert to numpy for computation
-        for delta in reversed(td_delta):  # Iterate from the end to the beginning
-            advantage = delta + self.gamma * self.lmbda * advantage
-            advantages.insert(0, advantage)
-        advantages = torch.FloatTensor(advantages).to(self.device)
+        # advantages = []
+        # advantage = 0.0
+        # td_delta = td_delta.cpu().detach().numpy()  # Convert to numpy for computation
+        # for delta in reversed(td_delta):  # Iterate from the end to the beginning
+        #     advantage = delta + self.gamma * self.lmbda * advantage
+        #     advantages.insert(0, advantage)
+        # advantages = torch.FloatTensor(advantages).to(self.device)
 
-        log_std = torch.zeros_like(self.actor(states)) + 0.2  # 假设固定标准差
-        std = torch.exp(log_std)
-        dist = torch.distributions.Normal(self.actor(states), std)
-        old_log_probs = dist.log_prob(actions).sum(dim=-1, keepdim=True).detach()
+        # log_std = torch.zeros_like(self.actor(states)) + 0.2  # 假设固定标准差
+        # std = torch.exp(log_std)
+        # dist = torch.distributions.Normal(self.actor(states), std)
+        # old_log_probs = dist.log_prob(actions).sum(dim=-1, keepdim=True).detach()
 
         for i in range(50):
-            dist = torch.distributions.Normal(self.actor(states), std)
+            dist = torch.distributions.Normal(self.actor(states), 0.2)
             log_probs = dist.log_prob(actions).sum(dim=-1, keepdim=True)
             ratio = torch.exp(log_probs - old_log_probs)
 
@@ -243,7 +256,7 @@ if __name__ == "__main__":
     
     batch_size = 512
     # episodes = int(5e6)
-    max_timsteps = 20000#int(500e3)
+    max_timsteps = int(5e6)#int(500e3)
     start_timesteps = 100 #int(25e3)
     episode_timesteps = 0
     episode_reward = 0
@@ -259,32 +272,26 @@ if __name__ == "__main__":
 
     for t in range(max_timsteps):
         episode_timesteps += 1
-
-        for i in range(300):
-            if t < start_timesteps:
-                action = env.action_space.sample()
-            else:
-                noise = 0.2
-                action = action = td3_agent.select_action(state=state)
-                action += noise * np.random.normal(size=action.shape)
-                action = np.clip(action, -1.0, 1.0)
             
-            next_state, reward, done, truncated, info = env.step(action)
-            next_state = np.concatenate([next_state[key].flatten() for key in ['observation', 'achieved_goal', 'desired_goal']])
-            done = torch.tensor(done, dtype=torch.bool)
-            truncated = torch.tensor(truncated, dtype=torch.bool)
-            done_bool = done
+        action = td3_agent.select_action(state=state)
+        next_state, reward, done, truncated, info = env.step(action)
+        next_state = np.concatenate([next_state[key].flatten() for key in ['observation', 'achieved_goal', 'desired_goal']])
+        done = torch.tensor(done, dtype=torch.bool)
+        truncated = torch.tensor(truncated, dtype=torch.bool)
+        done_bool = done
+        value = td3_agent.critic(torch.FloatTensor(state).to(device)).item()
+        log_prob = torch.distributions.Normal(torch.FloatTensor(action), 0.2).log_prob(torch.FloatTensor(action)).sum().item()
 
-            replay_buffer.add(state, action, next_state, reward, done_bool)
+        replay_buffer.add(state, action, next_state, reward, done_bool, value, log_prob)
 
-            state = next_state
-            episode_reward += reward
+        state = next_state
+        episode_reward += reward
 
-        # if t > start_timesteps:
-        td3_agent.train()
-        replay_buffer.clear()
         
         if (done or truncated):
+            td3_agent.train()
+            replay_buffer.clear()
+
             episode_rewards.append(episode_reward)
             if len(episode_rewards) > avg_episode_reward_window:
                 episode_rewards.pop(0)
